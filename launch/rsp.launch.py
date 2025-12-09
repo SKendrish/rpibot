0dddf98fcae843861d4d0071ba6aae04bfb8aad2
SKendrish <stephenkendrish@gmail.com>
2025-12-09 17:52:42 -0600

    robot description and urdf init

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,7 +3,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
@@ -15,18 +16,29 @@ def generate_launch_description():
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('rpibot'))
-    xacro_file = os.path.join(pkg_path,'description','rpibot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    # Load xacro file
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                   FindPackageShare("rpibot"),
+                   "description",
+                   "rpibot.urdf.xacro"
+                ]
+           ) 
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[robot_description, {'use_sim_time': use_sim_time}]
     )
 
 
